05a7e4ce7eb37fb094ce8bd1b1912ed8b4b97c40
Caleb <Tigers1025@gmail.com>
2022-09-27 11:53:57 -0400

    Solution

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,13 +1,9 @@
 class Audition < ActiveRecord::Base
   belongs_to :role
-
-  def self.role
-  self.role  
-  end
   
   
   def call_back
-  self.update(hired: true)
+  update(hired: true)
   end
 
 
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,32 @@
 class Role < ActiveRecord::Base
 has_many :auditions
 
+    def self.auditions
+    self.auditions
+    end
+
+    def actors
+    auditions.map {|audition| audition.actor}
+    end
+    
+    def locations
+    locations.map {|a| a.location}
+    end
+    
+    def lead
+        if auditions.where(hired: true).length > 0
+            auditions.where(hired: true)[0]
+        else 
+             "no actor has been hired for this role"
+        end
+    end
+
+    def understudy
+        if auditions.where(hired: true).length > 1
+            auditions.where(hired: true)[1]
+
+        else 
+             "no actor has been hired for understudy for this role"
+        end
+    end
 end
